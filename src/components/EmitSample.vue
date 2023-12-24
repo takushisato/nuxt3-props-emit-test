4c8d15e1e766a36335c95e78cfd31d24546abcce
takushisato <takuchanpower@gmail.com>
2023-12-24 13:25:58 +0900

    emit

---
+++ src/components/EmitSample.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="sendToParent">クリックして親に送信</button>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['sendEvent']);
+
+function sendToParent() {
+    emit('sendEvent', 'こんにちは、親コンポーネント!');
+}
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,16 +2,24 @@
     <div>
         <h1>Index</h1>
         <PropsSample :props-message="propsMessage" />
+        <EmitSample @sendEvent="handleEvent" />
     </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import EmitSample from '../components/EmitSample.vue';
 export default defineComponent({
     setup() {
         const propsMessage = '親からのメッセージ';
+        function handleEvent(message) {
+            console.log('子コンポーネントからのメッセージ:', message);
+        }
         return {
-            propsMessage
+            propsMessage,
+            handleEvent,
         };
-    }
+
+    },
+    components: { EmitSample }
 });
 </script>
