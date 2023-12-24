7be6aa5e697e4937fa2762f131d2e15f62bc4e67
takushisato <takuchanpower@gmail.com>
2023-12-24 10:27:40 +0900

    環境構築

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: false,
+  srcDir: 'src',
+  components: ['@/components', '@/components/parts', '@/components/blocks', '@/components/forms'],
+  modules: [],
+  // css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css', '@/assets/styles/style.scss'],
+  // build: {
+  //   transpile: ['vuetify'],
+  // },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Index</h1>
+    </div>
+</template>
+<script lang="ts"></script>
