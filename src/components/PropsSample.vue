484bed1ad87f0331acea24e2ad48a198a1b01794
takushisato <takuchanpower@gmail.com>
2023-12-24 12:01:38 +0900

    props

---
+++ src/components/PropsSample.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ propsMessage }}
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    props: {
+        propsMessage: {
+            propsMessage: String,
+            required: true
+        }
+    }
+});
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
         <h1>Index</h1>
+        <PropsSample :props-message="propsMessage" />
     </div>
 </template>
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    setup() {
+        const propsMessage = '親からのメッセージ';
+        return {
+            propsMessage
+        };
+    }
+});
+</script>
